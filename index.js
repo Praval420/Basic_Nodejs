4e1d83e0c3491b43437bdda48c02301ce5b099c1
Praval420 <sourav12345singhpraval@gmail.com>
2025-06-05 12:34:15 +0530

    final commit for dealing with forms

--- index.js
+++ index.js
@@ -15,6 +15,24 @@ app.get("/",function(req,res){
     });
 })
 
+app.get("/files/:filename",function(req,res){
+    fs.readFile(`./files/${req.params.filename}`,"utf-8",function(err,filedata){
+        res.render('show',{filename:req.params.filename,filedata:filedata});
+    })
+})
+
+app.get("/edit/:filename",function(req,res){
+    res.render('edit',{filename:req.params.filename});
+})
+
+app.post("/dost",function(req,res){
+    console.log(req.body);
+    fs.rename(`./files/${req.body.previous}`,`./files/${req.body.current}`,function(err){
+        res.redirect("/");
+    });
+
+})
+
 app.post("/login",function(req,res){
     fs.writeFile(`./files/${req.body.login.split(' ').join('')}.txt`,req.body.details,function(err){
         console.log("error found");
@@ -22,5 +40,15 @@ app.post("/login",function(req,res){
     res.redirect('/');
 })
 
+// app.get("/sourav", (req, res) => {
+//   res.render("hello");
+// });
+
+app.post("/sourav", (req, res) => {
+  const prev = req.body.previous;
+  const curr = req.body.current;
+  console.log(prev+curr);
+  res.render("hello",{prev,curr});
+});;
 
 app.listen(3000);
